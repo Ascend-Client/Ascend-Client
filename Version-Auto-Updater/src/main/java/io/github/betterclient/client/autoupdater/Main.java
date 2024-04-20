083ff5b2d751a83edbda15cbfa381ce88a448f97
betterclient <iamanaltlolwhypog@gmail.com>
2024-04-20 22:04:31 +0300

    it work

--- Version-Auto-Updater/src/main/java/io/github/betterclient/client/autoupdater/Main.java
+++ Version-Auto-Updater/src/main/java/io/github/betterclient/client/autoupdater/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class Main {
