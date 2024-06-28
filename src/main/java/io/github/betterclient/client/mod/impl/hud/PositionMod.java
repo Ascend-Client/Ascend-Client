ff18055f963928bacb813533e2ea1ca723cee08a
betterclient <betterclienthasacrazygmail@gmail.com>
2024-06-28 19:02:48 +0300

    fix position mod crash

--- src/main/java/io/github/betterclient/client/mod/impl/hud/PositionMod.java
+++ src/main/java/io/github/betterclient/client/mod/impl/hud/PositionMod.java
@@ -19,6 +19,23 @@ public class PositionMod extends HUDModule {
     @Override
     public void render(Renderable renderable) {
         IBridge.PlayerEntity entity = IBridge.MinecraftClient.getInstance().getPlayer();
+        if(entity == null) {
+            renderable.renderText("X: " + 100, 0, 0, textColor.getColor())
+                    .renderText("Y: " + 50, 0, 10, textColor.getColor())
+                    .renderText("Z: " + 200, 0, 20, textColor.getColor());
+
+            int cy = 30;
+
+            if(showBiome.value) {
+                renderable.renderText("Biome: Plains", 0, cy, textColor.getColor());
+                cy+=10;
+            }
+            if(showFacing.value) {
+                renderable.renderText("Facing: North", 0, cy, textColor.getColor());
+            }
+            return;
+        }
+
         IBridge.Vec3d pos = entity.getPos();
         renderable.renderText("X: " + ((int) pos.x), 0, 0, textColor.getColor())
                 .renderText("Y: " + ((int) pos.y), 0, 10, textColor.getColor())
